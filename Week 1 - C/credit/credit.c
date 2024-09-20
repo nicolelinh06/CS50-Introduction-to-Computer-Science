fe3dc1cad440cc5dd7289b0f18e5a1079d0e1290
Vũ Ngọc Linh <vungoclinhmi7@gmail.com>
2024-09-20 11:23:43 +0700

    Change to Week 1

--- Week 1 - C/credit/credit.c
+++ Week 1 - C/credit/credit.c
@@ -1,6 +1,29 @@
 # include<stdio.h>
+int check_sum (num);
 
 int main (void)
+{
+    int num;
+    do {
+        printf("Number");
+        scanf(" %i", &num);
+    }
+    while ();
+
+
+}
+
+int check_sum (num);
+{
+
+}
+
+int check_length (num);
+{
+    
+}
+
+int check_start (num);
 {
     
 }
